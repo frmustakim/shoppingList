d9af1b99fc72227c8b92400efd1480c9c72b22e8
frmustakim <md.fazle.rabbi@outlook.com>
2019-12-05 20:59:26 +0600

    Implemented Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Shopping List</h1>
-      <AppNavBar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Shopping List</h1>
+        <AppNavBar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
