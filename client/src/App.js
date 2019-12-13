e61bfbb39da79a2ea7b03ce4e0480eb27b70813a
frmustakim <md.fazle.rabbi@outlook.com>
2019-12-13 19:01:05 +0600

    Finishing with JWT

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,6 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Shopping List</h1>
         <AppNavBar />
         <Container>
           <ItemModal />
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,25 +4,23 @@ const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
 
-//User Model
+// User Model
 const User = require("../../models/User");
 
-//@route  POST api/users
-//@desc   Register users
-//@access Public
+// @route   POST api/users
+// @desc    Register new user
+// @access  Public
 router.post("/", (req, res) => {
   const { name, email, password } = req.body;
 
-  //simple validation
+  // Simple validation
   if (!name || !email || !password) {
-    return res.status(400).json({ msg: "Please fill up all fields" });
+    return res.status(400).json({ msg: "Please enter all fields" });
   }
 
-  //Check for existing user
+  // Check for existing user
   User.findOne({ email }).then(user => {
-    if (user) {
-      return res.status(400).json({ msg: "User already exists" });
-    }
+    if (user) return res.status(400).json({ msg: "User already exists" });
 
     const newUser = new User({
       name,
@@ -30,7 +28,7 @@ router.post("/", (req, res) => {
       password
     });
 
-    //Create salt & hash
+    // Create salt & hash
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
         if (err) throw err;
